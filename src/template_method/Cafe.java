f9964fd29737a7f40b26f059c972fc95b50bbdfe
RaphaAmericano <raphael@raphaelamericano.com.br>
2017-07-06 13:20:53 -0300

    Atualizando Objetos do  Template

--- src/template_method/Cafe.java
+++ src/template_method/Cafe.java
@@ -1,12 +1,20 @@
 package template_method;
 
-public class Cafe {
+public class Cafe extends BebidaCafeina{
 	void prepararReceita(){
 		ferverAgua();
 		colocarCafe();
 		colocarNaXicara();
 		adicionarAcucarLeite();
 	}
+	//
+	public void misturarProduto(){
+		System.out.println("Misturando chá");
+	}
+	public void adicionarCondimentos(){
+		System.out.println("Adicionando limão");
+	}
+	//
 	public void ferverAgua(){
 		
 	}
--- src/template_method/Cha.java
+++ src/template_method/Cha.java
@@ -7,6 +7,13 @@ public class Cha extends BebidaCafeina{
 		colocarNaXicara();
 		adicionarLimao();
 	}	
+	public void misturarProduto(){
+		System.out.println("Misturar café");
+	}
+	public void adicionarCondimentos(){
+		System.out.println("Adicionando açucar e leite");
+	}
+	//----
 	public void ferverAgua(){
 		
 	}
