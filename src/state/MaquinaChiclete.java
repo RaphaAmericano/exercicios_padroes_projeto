338c67267ec0ccd2dc85483f93fe18a7f55ebbbd
RaphaAmericano <raphael@raphaelamericano.com.br>
2017-07-07 14:40:56 -0300

    Metodo ejetarMoeda

--- src/state/MaquinaChiclete.java
+++ src/state/MaquinaChiclete.java
@@ -1,10 +1,10 @@
 package state;
 
 public class MaquinaChiclete {
-	final static int SOLD_OUT = 0;
-	final static int NO_QUARTER = 1;
-	final static int HAS_QUARTER = 2;
-	final static int SOLD = 3;
+	final static int CHICLETE_VENDIDO = 0;
+	final static int MAQUINA_VAZIA = 1;
+	final static int TEM_MOEDA = 2;
+	final static int SEM_MOEDA = 3;
 	
 	int state = SOLD_OUT;
 	int count = 0;
@@ -12,7 +12,7 @@ public class MaquinaChiclete {
 	public MaquinaChiclete(int count ){
 		this.count = count;
 		if( count > 0 ){
-			state = NO_QUARTER;
+			state = TEM_MOEDA;
 		}
 	}
 	public void inserirMoeda(){
@@ -25,6 +25,20 @@ public class MaquinaChiclete {
 		} else if (state == SEM_MOEDA){
 			state = TEM_MOEDA;
 			println("Você inseriu uma moeda");
+		}
+	}
+	public void ejetarMoeda(){
+		if(state == TEM_MOEDA ){
+			System.out.println("Moeda voltou");
+			state = SEM_MOEDA;
+		} else if (state == SEM_MOEDA){
+			System.out.println("Duas vezes não te darão outro ciclete");
+		} else if ( state == MAQUINA_VAZIA){
+			System.out.println("Não há chiclete");
+		} else if (state == TEM_MOEDA){
+			System.out.println("VocÊ virou..");
+			state = SOLD;
+			dispense();
 		}
 	}
 }
